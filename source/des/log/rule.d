2188a873a664ccdbb388d594324480f39c745134
deviator <code.viator@gmail.com>
2015-12-12 11:49:55 +0300

    fix registred flag, optimize allowedLevel call

--- source/des/log/rule.d
+++ source/des/log/rule.d
@@ -14,8 +14,9 @@ package:
     LogLevel level = LogLevel.ERROR; ///
     shared Rule[string] inner; ///
 
-    bool use_minimal = true; ///
-    bool only_register = false; ///
+    bool use_minimal = true;
+    bool only_register = false;
+    bool registred = false;
 
 public:
 
@@ -23,8 +24,7 @@ public:
     this( shared Rule parent = null )
     {
         this.parent = parent;
-        if( parent )
-            this.level = parent.level;
+        if( parent ) this.level = parent.level;
     }
 
     @property
@@ -32,99 +32,104 @@ public:
         ///
         bool useMinimal() const
         {
-            if( parent !is null )
-                return parent.useMinimal;
+            if( parent !is null ) return parent.useMinimal;
             else return use_minimal;
         }
 
         ///
         bool useMinimal( bool v )
         {
-            if( parent !is null )
-                return parent.useMinimal = v;
+            if( parent !is null ) return parent.useMinimal = v;
             else return use_minimal = v;
         }
 
         ///
         bool onlyRegister() const
         {
-            if( parent !is null )
-                return parent.onlyRegister;
+            if( parent !is null ) return parent.onlyRegister;
             else return only_register;
         }
 
         ///
         bool onlyRegister( bool v )
         {
-            if( parent !is null )
-                return parent.onlyRegister = v;
+            if( parent !is null ) return parent.onlyRegister = v;
             else return only_register = v;
         }
     }
 
     /// setting allowed level for emitter (create new inner Rule), if emitter is "" sets self level
     void setLevel( LogLevel lvl, string emitter="" )
-    {
-        auto addr = splitAddress( emitter );
-        if( addr[0].length == 0 ) { level = lvl; return; }
-        auto iname = addr[0];
-        if( iname !in inner ) inner[iname] = new shared Rule(this);
-        inner[iname].setLevel( lvl, addr[1] );
-    }
+    { setLevelImpl( lvl, emitter.split(".") ); }
 
     /// if emitter is "" returns self level
     LogLevel allowedLevel( string emitter="" ) const
-    {
-        auto addr = splitAddress( emitter );
-        if( addr[0].length == 0 ) return level;
-        auto iname = addr[0];
-
-        if( iname !in inner )
-        {
-            import des.log.consolecolor;
-            pragma(msg, CEColor.FG_B_RED, "TODO: onlyRegister not works", CEColor.OFF, __FILE__, ":", __LINE__ );
-            //TODO:
-            //if( onlyRegister )
-            //    return LogLevel.OFF;
-            //else
-                return level;
-        }
-
-        if( useMinimal )
-            return min( level, inner[iname].allowedLevel( addr[1] ) );
-        else
-            return inner[iname].allowedLevel( addr[1] );
-    }
+    { return allowedLevelImpl( emitter.split(".") ); }
 
     /// test is message allowed for this rule
     bool isAllowed( in LogMessage lm ) const
     { return allowedLevel( lm.emitter ) >= lm.level; }
 
     /// return string what represent of rule structure
-    string strRepresent() const { return implStrRepresent(); }
+    string strRepresent() const { return strRepresentImpl(); }
 
 protected:
 
-    string implStrRepresent( string offset="", bool first=true ) const
+    void setLevelImpl( LogLevel lvl, string[] emitter )
     {
-        string ret = format( "%s", level );
-        if( first )
-            ret ~= format( " use min: %s, only register: %s",
-                useMinimal, onlyRegister );
-        foreach( key, val; inner )
-            ret ~= format( "\n%s%s : %s", offset, key,
-                    val.implStrRepresent( offset ~ mlt(" ",key.length), false ) );
-        return ret;
+        if( emitter.length == 0 || emitter[0].length == 0 )
+        {
+            level = lvl;
+            registred = true;
+            return;
+        }
+
+        auto iname = emitter[0];
+
+        if( iname !in inner )
+            inner[iname] = new shared Rule(this);
+
+        inner[iname].setLevelImpl( lvl, emitter.length > 1 ? emitter[1..$] : [] );
     }
 
-    ///
-    static string[2] splitAddress( string emitter )
+    /// if emitter is "" returns self level
+    LogLevel allowedLevelImpl( string[] emitter ) const
     {
-        auto addr = emitter.split(".");
-        if( addr.length == 0 ) return ["",""];
-        if( addr.length == 1 ) return [addr[0],""];
+        if( emitter.length == 0 || emitter[0].length == 0 )
+            return level;
 
-        return [ addr[0], addr[1..$].join(".") ];
+        auto iname = emitter[0];
+
+        if( iname !in inner )
+        {
+            if( onlyRegister )
+            {
+                if( !(registred && parent !is null) )
+                    return LogLevel.OFF;
+                else
+                    return level;
+            }
+            else return level;
+        }
+
+        auto childnames = emitter.length > 1 ? emitter[1..$] : [];
+
+        if( useMinimal )
+            return min( level, inner[iname].allowedLevelImpl( childnames ) );
+        else
+            return inner[iname].allowedLevelImpl( childnames );
+    }
+
+    string strRepresentImpl( string offset="", bool first=true ) const
+    {
+        string ret = format( "%s%s", level, registred ? " [reg]" : "" );
+        if( first )
+            ret ~= format( "%s%s", useMinimal ? " use minimal" : "",
+                                 onlyRegister ? " only register" : "" );
+        foreach( key, val; inner )
+            ret ~= format( "\n%s%s : %s", offset, key,
+                    val.strRepresentImpl( offset ~ mlt(" ",key.length), false ) );
+        return ret;
     }
 }
 
